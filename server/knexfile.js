fb92f5a8d127044d830bce3f4499a8f8098a68db
Caleb Cross <arkansashurdler@gmail.com>
2021-04-15 08:21:44 -0400

    first commit

---
+++ server/knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: "mysql",
+  connection: {
+    host: "***REMOVED***",
+    user: ***REMOVED***,
+    password: ***REMOVED***,
+    database: ***REMOVED***,
+    charset: "utf8"
+  }
+};
---
+++ server/seed_data/states.js
@@ -0,0 +1,392 @@
+module.exports = [
+  {
+    "name": "Alaska",
+    "stateCode": "AK",
+    "region": "West",
+    "division": "Pacific"
+  },
+  {
+    "name": "Alabama",
+    "stateCode": "AL",
+    "region": "South",
+    "division": "East South Central"
+  },
+  {
+    "name": "American Samoa",
+    "stateCode": "AS",
+    "region": "Pacific",
+    "division": "Territory"
+  },
+  {
+    "name": "Arkansas",
+    "stateCode": "AR",
+    "region": "South",
+    "division": "West South Central"
+  },
+  {
+    "name": "Arizona",
+    "stateCode": "AZ",
+    "region": "West",
+    "division": "Mountain"
+  },
+  {
+    "name": "Bureau of Prisons",
+    "stateCode": "BP",
+    "region": "West",
+    "division": "Mountain"
+  },
+  {
+    "name": "California",
+    "stateCode": "CA",
+    "region": "West",
+    "division": "Pacific"
+  },
+  {
+    "name": "Colorado",
+    "stateCode": "CO",
+    "region": "West",
+    "division": "Mountain"
+  },
+  {
+    "name": "Connecticut",
+    "stateCode": "CT",
+    "region": "Northeast",
+    "division": "New England"
+  },
+  {
+    "name": "District of Columbia",
+    "stateCode": "DC",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "Delaware",
+    "stateCode": "DE",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "Dept of Defense",
+    "stateCode": "DD",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "Federated States of Micronesia",
+    "stateCode": "FSM",
+    "region": "South Pacific",
+    "division": "Pacific"
+  },
+  {
+    "name": "Florida",
+    "stateCode": "FL",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "Georgia",
+    "stateCode": "GA",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "Guam",
+    "stateCode": "GM",
+    "region": "Pacific",
+    "division": "South Pacific"
+  },
+  {
+    "name": "Hawaii",
+    "stateCode": "HI",
+    "region": "West",
+    "division": "Pacific"
+  },
+  {
+    "name": "Iowa",
+    "stateCode": "IA",
+    "region": "Midwest",
+    "division": "West North Central"
+  },
+  {
+    "name": "Idaho",
+    "stateCode": "ID",
+    "region": "West",
+    "division": "Mountain"
+  },
+  {
+    "name": "Illinois",
+    "stateCode": "IL",
+    "region": "Midwest",
+    "division": "East North Central"
+  },
+  {
+    "name": "Indiana",
+    "stateCode": "IN",
+    "region": "Midwest",
+    "division": "East North Central"
+  },
+  {
+    "name": "Indian Health Svc",
+    "stateCode": "IHS",
+    "region": "Midwest",
+    "division": "?"
+  },
+  {
+    "name": "Kansas",
+    "stateCode": "KS",
+    "region": "Midwest",
+    "division": "West North Central"
+  },
+  {
+    "name": "Kentucky",
+    "stateCode": "KY",
+    "region": "South",
+    "division": "East South Central"
+  },
+  {
+    "name": "Long Term Care",
+    "stateCode": "LTC",
+    "region": "South",
+    "division": "West South Central"
+  },
+  {
+    "name": "Louisiana",
+    "stateCode": "LA",
+    "region": "South",
+    "division": "West South Central"
+  },
+  {
+    "name": "Massachusetts",
+    "stateCode": "MA",
+    "region": "Northeast",
+    "division": "New England"
+  },
+  {
+    "name": "Marshall Islands",
+    "stateCode": "MI",
+    "region": "Pacific",
+    "division": "South Pacific"
+  },
+  {
+    "name": "Maryland",
+    "stateCode": "MD",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "Maine",
+    "stateCode": "ME",
+    "region": "Northeast",
+    "division": "New England"
+  },
+  {
+    "name": "Michigan",
+    "stateCode": "MI",
+    "region": "Midwest",
+    "division": "East North Central"
+  },
+  {
+    "name": "Minnesota",
+    "stateCode": "MN",
+    "region": "Midwest",
+    "division": "West North Central"
+  },
+  {
+    "name": "Missouri",
+    "stateCode": "MO",
+    "region": "Midwest",
+    "division": "West North Central"
+  },
+  {
+    "name": "Mississippi",
+    "stateCode": "MS",
+    "region": "South",
+    "division": "East South Central"
+  },
+  {
+    "name": "Montana",
+    "stateCode": "MT",
+    "region": "West",
+    "division": "Mountain"
+  },
+  {
+    "name": "North Carolina",
+    "stateCode": "NC",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "North Dakota",
+    "stateCode": "ND",
+    "region": "Midwest",
+    "division": "West North Central"
+  },
+  {
+    "name": "Nebraska",
+    "stateCode": "NE",
+    "region": "Midwest",
+    "division": "West North Central"
+  },
+  {
+    "name": "New Hampshire",
+    "stateCode": "NH",
+    "region": "Northeast",
+    "division": "New England"
+  },
+  {
+    "name": "New Jersey",
+    "stateCode": "NJ",
+    "region": "Northeast",
+    "division": "Middle Atlantic"
+  },
+  {
+    "name": "New Mexico",
+    "stateCode": "NM",
+    "region": "West",
+    "division": "Mountain"
+  },
+  {
+    "name": "Nevada",
+    "stateCode": "NV",
+    "region": "West",
+    "division": "Mountain"
+  },
+  {
+    "name": "New York",
+    "stateCode": "NY",
+    "region": "Northeast",
+    "division": "Middle Atlantic"
+  },
+  {
+    "name": "Northern Mariana Islands",
+    "stateCode": "NMI",
+    "region": "Pacific",
+    "division": "South Pacific"
+  },
+  {
+    "name": "Ohio",
+    "stateCode": "OH",
+    "region": "Midwest",
+    "division": "East North Central"
+  },
+  {
+    "name": "Oklahoma",
+    "stateCode": "OK",
+    "region": "South",
+    "division": "West South Central"
+  },
+  {
+    "name": "Oregon",
+    "stateCode": "OR",
+    "region": "West",
+    "division": "Pacific"
+  },
+  {
+    "name": "Pennsylvania",
+    "stateCode": "PA",
+    "region": "Northeast",
+    "division": "Middle Atlantic"
+  },
+  {
+    "name": "Puerto Rico",
+    "stateCode": "PR",
+    "region": "Atlantic",
+    "division": "Territory"
+  },
+  {
+    "name": "Republic of Palau",
+    "stateCode": "RP",
+    "region": "Pacific",
+    "division": "South Pacific"
+  },
+  {
+    "name": "Rhode Island",
+    "stateCode": "RI",
+    "region": "Northeast",
+    "division": "New England"
+  },
+  {
+    "name": "South Carolina",
+    "stateCode": "SC",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "South Dakota",
+    "stateCode": "SD",
+    "region": "Midwest",
+    "division": "West North Central"
+  },
+  {
+    "name": "Tennessee",
+    "stateCode": "TN",
+    "region": "South",
+    "division": "East South Central"
+  },
+  {
+    "name": "Texas",
+    "stateCode": "TX",
+    "region": "South",
+    "division": "West South Central"
+  },
+  {
+    "name": "Utah",
+    "stateCode": "UT",
+    "region": "West",
+    "division": "Mountain"
+  },
+  {
+    "name": "United States",
+    "stateCode": "US",
+    "region": "North America",
+    "division": "Americas"
+  },
+  {
+    "name": "Virginia",
+    "stateCode": "VA",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "Virgin Islands",
+    "stateCode": "VI",
+    "region": "Carribean",
+    "division": "Territory"
+  },
+  {
+    "name": "Vermont",
+    "stateCode": "VT",
+    "region": "Northeast",
+    "division": "New England"
+  },
+  {
+    "name": "Veterans Health",
+    "stateCode": "VH",
+    "region": "IRDC",
+    "division": "IDC"
+  },
+  {
+    "name": "Washington",
+    "stateCode": "WA",
+    "region": "West",
+    "division": "Pacific"
+  },
+  {
+    "name": "Wisconsin",
+    "stateCode": "WI",
+    "region": "Midwest",
+    "division": "East North Central"
+  },
+  {
+    "name": "West Virginia",
+    "stateCode": "WV",
+    "region": "South",
+    "division": "South Atlantic"
+  },
+  {
+    "name": "Wyoming",
+    "stateCode": "WY",
+    "region": "West",
+    "division": "Mountain"
+  }
+ ]
