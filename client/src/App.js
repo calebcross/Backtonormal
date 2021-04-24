496f6cdf211f141663389c550c26ca3339287bc5
Caleb Cross <arkansashurdler@gmail.com>
2021-04-23 22:35:38 -0400

    first fetch from working graphql api

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,58 @@
+import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+//components
+import Partially from "./components/Partially";
+import Fully from "./components/Fully";
+//style
+import "./scss/custom.scss";
 
-import './scss/custom.scss';
-import Partially from './components/Partially'
-import Fully from './components/Fully'
+
+/* const client = new ApolloClient({
+	uri: "http://localhost:8080/graphql",
+	cache: new InMemoryCache(),
+}); */
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+  cache: new InMemoryCache()
+});
+
+
+const getStateQuery = gql`
+{
+  entry (date: "4/12/21" state: "United States") {
+    date
+    people_vaccinated
+    people_fully_vaccinated
+  }
+  
+}`
 
 function App() {
-  return (
-    <div className="d-flex flex-column justify-content-center my-5 mx-2">
-    <Partially title="AT LEAST PARTIALLY VACCINATED" />
-    <Fully title= "FULLY VACCINATED" />
-    </div>
-  );
+
+   const { loading, error, data } = useQuery(getStateQuery);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+    console.log(data) 
+
+	return (
+		<ApolloProvider client={client}>
+			<div className='d-flex flex-column justify-content-center my-5 mx-2'>
+				<h1 className='display-6 text-center'>
+					<strong>
+						Days until
+						<br />
+						normal:
+						<br />
+						88
+					</strong>
+				</h1>
+				<Partially title='AT LEAST PARTIALLY VACCINATED' />
+				<Fully title='FULLY VACCINATED' />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- client/src/components/Partially.jsx
+++ client/src/components/Partially.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 function Partially({ title }) {
 	return (
-		<div>
+		<div className="partially">
 			<div className='card border-dark mb-3'>
-				<div className='card-header svg'>{title}</div>
-				<div className='d-flex justify-content-evenly'>
-					<div className='card-body text-center text-warning'>
-						<h4 className='card-title fw-bold'>2,000,000</h4>
-						<p className='card-text '>persons</p>
+				<div className='card-header card-header-vcenter'>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="syringe" className="svg-inline--fa fa-syringe fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M201.5 174.8l55.7 55.8c3.1 3.1 3.1 8.2 0 11.3l-11.3 11.3c-3.1 3.1-8.2 3.1-11.3 0l-55.7-55.8-45.3 45.3 55.8 55.8c3.1 3.1 3.1 8.2 0 11.3l-11.3 11.3c-3.1 3.1-8.2 3.1-11.3 0L111 265.2l-26.4 26.4c-17.3 17.3-25.6 41.1-23 65.4l7.1 63.6L2.3 487c-3.1 3.1-3.1 8.2 0 11.3l11.3 11.3c3.1 3.1 8.2 3.1 11.3 0l66.3-66.3 63.6 7.1c23.9 2.6 47.9-5.4 65.4-23l181.9-181.9-135.7-135.7-64.9 65zm308.2-93.3L430.5 2.3c-3.1-3.1-8.2-3.1-11.3 0l-11.3 11.3c-3.1 3.1-3.1 8.2 0 11.3l28.3 28.3-45.3 45.3-56.6-56.6-17-17c-3.1-3.1-8.2-3.1-11.3 0l-33.9 33.9c-3.1 3.1-3.1 8.2 0 11.3l17 17L424.8 223l17 17c3.1 3.1 8.2 3.1 11.3 0l33.9-34c3.1-3.1 3.1-8.2 0-11.3l-73.5-73.5 45.3-45.3 28.3 28.3c3.1 3.1 8.2 3.1 11.3 0l11.3-11.3c3.1-3.2 3.1-8.2 0-11.4z"></path></svg>
+                <strong>{title}</strong></div>
+				<div className='d-flex justify-content-evenly flex-wrap'>
+					<div className='card-body text-center '>
+						<h3 className='card-title fw-bold'><strong>3,045,000</strong></h3>
+						<p className='card-text '><strong>persons</strong></p>
 					</div>
-					<div className='card-body text-center text-warning'>
-						<h4 className='card-title fw-bold'>14.23%</h4>
-						<p className='card-text text-center'>of the population</p>
+					<div className='card-body text-center'>
+						<h3 className='card-title fw-bold'><strong>5.27%</strong></h3>
+						<p className='card-text text-center'><strong>of the population</strong></p>
 					</div>
 				</div>
 			</div>
