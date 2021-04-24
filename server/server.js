f8fcc067267a6474703942057b19296fed5547a9
Caleb Cross <arkansashurdler@gmail.com>
2021-04-23 22:35:38 -0400

    first fetch from working graphql api

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const cors = require("cors");
+
 const app = express();
 
+
 const APP_PORT = 8080;
+
+app.use(cors())
 
 app.use(
 	"/graphql",
