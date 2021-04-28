d939dc13431fd865a92d2107aeca2e004622e114
Caleb Cross <arkansashurdler@gmail.com>
2021-04-28 10:14:54 -0400

    add chart.js

--- client/src/components/VacChart.jsx
+++ client/src/components/VacChart.jsx
@@ -1,51 +1,53 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Chart } from "chart.js";
+import 'chartjs-adapter-date-fns';
 import { Bar, Line, Pie } from "react-chartjs-2";
+import { ResponsiveLine } from "@nivo/line";
 
 function VacChart() {
-	// set data
-	const [barData, setBarData] = useState({
-		labels: ["label 1", "label 2", "label 3", "label 4"],
-		datasets: [
-			{
-				label: "test label",
-				data: [48, 35, 73, 82],
-				backgroundColor: [
-					"rgba(255, 99, 132, 0.6)",
-					"rgba(54, 162, 235, 0.6)",
-					"rgba(255, 206, 86, 0.6)",
-					"rgba(75, 192, 192, 0.6)",
-				],
-				borderWidth: 3,
+
+	const labels = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+	  ];
+
+	const data = {
+		labels: labels,
+		datasets: [{
+		  label: 'My First dataset',
+		  backgroundColor: 'rgb(231,76,60)',
+		  borderColor: 'rgb(231,76,60)',
+		  data: [0, 10, 5, 2, 20, 30, 45],
+		}]
+	  }
+
+	useEffect(() => {
+		const ctx = document.getElementById("myChart");
+		new Chart(ctx, {
+			type: 'line',
+			data: data,
+			options: {
+			  scales: {
+				y: {
+				  beginAtZero: true
+				}
+			  }
 			},
-		],
-	});
-	// set options
-	const [barOptions, setBarOptions] = useState({
-		options: {
-			scales: {
-				yAxes: [
-					{
-						ticks: {
-							beginAtZero: true,
-						},
-					},
-				],
-			},
-			title: {
-				display: true,
-				text: "Data Orgranized In Bars",
-				fontSize: 25,
-			},
-			legend: {
-				display: true,
-				position: "top",
-			},
-		},
-	});
+		  });
+	  });
+
+
+	
 	return (
-		<div className='BarExample'>
-			<Bar data={barData} options={barOptions} />
+		<div class='card border-dark mb-3'>
+			<div class='card-header text-center'>Vaccinated Percentage of the Population</div>
+			<div class='card-body'>
+			<canvas id="myChart" width="400" />
+			</div>
 		</div>
 	);
 }
