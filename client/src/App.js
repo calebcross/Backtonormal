315a880d28e496b01bf5743f738cc534bc92d12a
Caleb Cross <arkansashurdler@gmail.com>
2021-04-28 10:14:54 -0400

    add chart.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,12 +20,22 @@ const getInfo = gql`
     
   }
 `;
+const getChartInfo = gql`
+  query GetInfo  {
+    entries (date:"2021-04-26" ){
+  
+      People_Fully_Vaccinated
+      People_with_at_least_One_Dose
+    }
+    
+  }
+`;
 
 function App() {
 
   const { loading, error, data } = useQuery(getInfo);
 
-  let population = 338472604;
+  let populationUs = 338472604;
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -33,13 +43,10 @@ function App() {
 	return (
 
 			<div className='d-flex flex-column justify-content-center my-5 mx-2'>
-				<Time />
-				<Partially title='partially vaccinated' data={data} population={population} />
-				<Fully title='fully vaccinated' data={data} population={population} />
-        <VacChart />
-        <VacChart />
-        <VacChart />
-        <VacChart />
+				<Time population={populationUs} />
+				<Partially title='partially vaccinated' data={data} population={populationUs} />
+				<Fully title='fully vaccinated' data={data} population={populationUs} />
+        <VacChart/>
 			</div>
 
 	);
