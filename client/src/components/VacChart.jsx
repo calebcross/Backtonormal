a84ad475662b5a3134b63a4482bea084a58e2270
Caleb Cross <arkansashurdler@gmail.com>
2021-05-02 01:08:09 -0400

    pargraph stylings for pct of pop

--- client/src/components/VacChart.jsx
+++ client/src/components/VacChart.jsx
@@ -1,12 +1,14 @@
-//import React, { useEffect, useRef, useState } from "react";
 import { useQuery, gql } from "@apollo/client";
-
-//import { Chart } from "chart.js";
-import { Line } from "react-chartjs-2";
+import { Bar } from 'react-chartjs-2';
+import "chartjs-plugin-labels";
+import "chartjs-plugin-datalabels";
+import { format } from 'date-fns';
+import { round } from 'mathjs';
+//import "chartjs-plugin-crosshair";
 
 const getChartInfo = gql`
 	query GetChartInfo {
-		entriesBy(state: "United States", from: "2021-04-01", to: "2021-04-28") {
+		entriesBy(state: "United States", from: "2021-03-08", to: "2021-04-28") {
 			date
 			Administered_Dose1_Pop_Pct
 			Series_Complete_Pop_Pct
@@ -28,11 +30,19 @@ const findDates = ({ entriesBy }) => {
 		return x-y
 	})
 
-	return newLabels.map((label) => label.split('2021-').[1]);
+	return newLabels.reduce( (renderArr, label, i) => {
+		if (i % 7 === 0) {
+			//renderArr.push(label.split('2021-').[1])
+			renderArr.push( format( new Date (label), 'MMM do' ) )
+			//renderArr.push( formatDistance( new Date (label), new Date() ) )
+			}
+			return renderArr
+	}, [])
 };
 
 const pluck = ({ entriesBy }, key) => {
-	let newArr = [...entriesBy].sort((a, b) => {
+	let newArr = [] 
+	newArr = [...entriesBy].sort((a, b) => {
 
 		if (a.date < b.date) {
 			return -1;
@@ -43,12 +53,40 @@ const pluck = ({ entriesBy }, key) => {
 		return 0;
 	});
 
-	return newArr.map((entry) => {
-		return entry[key];
+
+
+	return newArr.reduce( (renderArr, entry, i) => {
+		if (i % 7 === 0) {
+			renderArr.push(entry[key])
+			}
+			return renderArr
+	}, []) ;
+};
+const plucky = ({ entriesBy }, key, minus) => {
+	let newArr = [] 
+	newArr = [...entriesBy].sort((a, b) => {
+
+		if (a.date < b.date) {
+			return -1;
+		}
+		if (a.date > b.date) {
+			return 1;
+		}
+		return 0;
 	});
+
+
+
+	return newArr.reduce( (renderArr, entry, i) => {
+		if (i % 7 === 0) {
+			renderArr.push(entry[key] - entry[minus])
+			}
+			return renderArr
+	}, []) ;
 };
 
 function VacChart() {
+
 	const { loading, error, data } = useQuery(getChartInfo);
 
 	if (loading) return <p>Loading...</p>;
@@ -61,66 +99,67 @@ function VacChart() {
 		labels: findDates(data),
 		datasets: [
 			{
+				type: 'bar',
+				label: "Fully Vaccinated",
+				 backgroundColor: "rgb(187,222,251)",
+				borderColor: "rgb(187,222,251)",
+				data: pluck(data, [fully]),
+			},
+			{
+				type: 'bar',
 				label: "Partially Vaccinated",
 				backgroundColor: "rgb(255,183,78)",
 				borderColor: "rgb(255,183,78)",
 				data: pluck(data, [partially]),
 			},
-			{
-				label: "Fully Vaccinated",
-				backgroundColor: "rgb(187,222,251)",
-				borderColor: "rgb(187,222,251)",
-				data: pluck(data, [fully]),
-			},
+			
 		],
 	};
 
 	const options = {
- 		plugins: {
-			legend: {
-				display: true,
-				labels: {
-					color: "#fff",
+		plugins: {
+			labels: [{
+				render: (args) => {return /* round( args.value , 3) + '%' */},
+				fontColor: "black",
+				fontStyle: 'bold'
+			  }],
+			  datalabels: {
+				color: 'black',
+				display: function(context) {
+				  return `${context.dataset.data[context.dataIndex]}%`;
 				},
-			},
-			crosshair: {
-				line: {
-				  color: '#F66',        // crosshair line color
-				  width: 1,             // crosshair line width
-				  dashPattern: [5, 5]   // crosshair line dash pattern
+				font: {
+				  weight: 'normal'
 				},
-				sync: {
-				  enabled: true,            // enable trace line syncing with other charts
-				  group: 1,                 // chart group
-				  suppressTooltips: false   // suppress tooltips when showing a synced tracer
-				},
+				formatter: function(value) {
+					return `${value}%`;
+				  }
+			  }
 		},
-	}, 
-		scales: {
-			y: {
-				suggestedMax: 50,
-				ticks: {
-					callback: function (val, index) {
-						return val + "%";
-					},
-					color: "white",
-				},
-				grid: {
-					color: "#444",
-				},
-			},
-			x: {
-				ticks: {
-					callback: function(val, index) {
-						return index % 2 === 0 ? this.getLabelForValue(val) : '';
-					  },
-					color: "white",
-				},
-				grid: {
-					color: "#444",
-				},
-			},
-		},
+		  scales: {
+            yAxes: [{
+				stacked: true,
+                ticks: {
+                    callback: function(value, index, values) {
+                        return `${value}%`;
+                    },
+					fontColor: "white"
+                },
+				gridLines: {
+					color: "#444"
+				}
+            }],
+            xAxes: [{
+				stacked: true,
+                ticks: {
+					fontColor: "white"
+                },
+				gridLines: {
+					color: "#444"
+				}
+            }]
+        },
+		
 	};
 
 	return (
@@ -129,10 +168,37 @@ function VacChart() {
 				Vaccinated Percentage of the Population
 			</div>
 			<div className='card-body'>
-				<Line data={chartData} options={options} />
+				<Bar data={chartData} options={options} />
 			</div>
 		</div>
 	);
 }
 
 export default VacChart;
+
+
+/* scales: {
+	y: {
+		suggestedMax: 50,
+		ticks: {
+			callback: function (val, index) {
+				return val + "%";
+			},
+			color: "white",
+		},
+		grid: {
+			color: "#444",
+		},
+	},
+	x: {
+		ticks: {
+			callback: function(val, index) {
+				return index % 3 === 0 ? this.getLabelForValue(val) : '';
+			  },
+			color: "white",
+		},
+		grid: {
+			color: "#444",
+		},
+	},
+}, */
