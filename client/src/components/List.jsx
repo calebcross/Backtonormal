ed67bf0035d9213b6275177a616f9044b4aba41c
Caleb Cross <arkansashurdler@gmail.com>
2021-05-02 03:43:30 -0400

    fixed merge conflict with heroku

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -52,7 +52,6 @@ function List() {
 					renderArr.push(
 						<Card key={i}>
 							<Accordion.Toggle as={Card.Header} eventKey={i + 1}>
-<<<<<<< HEAD
 								<svg
 									aria-hidden='true'
 									focusable='false'
@@ -66,9 +65,6 @@ function List() {
 										fill='currentColor'
 										d='M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z'></path>
 								</svg>
-=======
-							<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" className="svg-inline--fa fa-chevron-down fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>
->>>>>>> heroku
 								{name}
 							</Accordion.Toggle>
 							<Accordion.Collapse eventKey={i + 1}>
