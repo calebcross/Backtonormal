c5d84edde6c1801ceb104558824fa52404c1d7f0
Caleb Cross <arkansashurdler@gmail.com>
2021-05-10 10:50:25 -0400

    add .env file

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,14 @@
 
 const moment = require('moment');
+require('dotenv').config({path: 'heroku.env'});
 
 module.exports = {
   client: "mysql",
   connection: {
-    host: "***REMOVED***",
-    user: "***REMOVED***",
-    password: "***REMOVED***",
-    database: "***REMOVED***",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB,
     charset: "utf8",
     typeCast: function (field, next) {
       if (field.type == 'DATE') {
