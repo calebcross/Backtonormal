c71639a1a4cdbcbb400dd901c8c2025c5f8f5476
Caleb Cross <arkansashurdler@gmail.com>
2021-05-11 13:11:06 -0400

    call to cdc api

--- server.js
+++ server.js
@@ -1,16 +1,48 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
-const cors = require("cors");
 const path = require("path");
+const axios = require('axios');
+const knex = require("knex")(require("./knexfile"));
 require('dotenv').config();
+
+
 
 const app = express();
 
 
-const APP_PORT = process.env.PORT || 8080;
+const CDC_URL = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_data";
 
-//app.use(cors())
+ async function scrape (URL) {
+
+	let res = await axios.get(URL)
+	
+return res.data.vaccination_data
+}
+
+const dailyData = scrape(CDC_URL)
+
+console.log(dailyData)
+
+ function insertDB (data) {
+
+	 console.log(data)
+
+	const cleansedData = data.map( entry => {
+		entry['Date'] = entry['date']
+		entry['LongName'] = entry['name']
+		entry['Census2019'] = entry['Census']
+	})
+
+  knex('entries').insert(cleansedData).then( (res) =>
+console.log(res))
+
+}
+
+//insertDB(dailyData)
+
+
+const APP_PORT = process.env.PORT || 8080;
 
 app.use(
 	"/graphql",
