16ea94239bbd9e32b492d769afd785f0bbb06794
Caleb Cross <arkansashurdler@gmail.com>
2021-05-11 16:49:03 -0400

    cleaning data from CDC api response

--- server.js
+++ server.js
@@ -2,45 +2,67 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const path = require("path");
-const axios = require('axios');
+const axios = require("axios");
 const knex = require("knex")(require("./knexfile"));
-require('dotenv').config();
-
-
+require("dotenv").config();
 
 const app = express();
 
+const CDC_URL =
+	"https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_data";
 
-const CDC_URL = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_data";
+async function scrape(URL) {
+	const response = await axios.get(URL);
+	//console.log(response.data.vaccination_data)
 
- async function scrape (URL) {
+	const { vaccination_data } = response.data;
 
-	let res = await axios.get(URL)
-	
-return res.data.vaccination_data
+	const cleansedData = vaccination_data.map((entry) => {
+		const {
+			Date,
+			LongName,
+			Census2019,
+			Location,
+			ShortName,
+			date_type,
+			Administered_Fed_LTC,
+			Administered_Fed_LTC_Dose1,
+			Administered_Fed_LTC_Dose2,
+			Series_Complete_FedLTC,
+			...restEntry
+		} = entry;
+
+		const newEntry = {
+			date: Date,
+			name: LongName,
+			Census: Census2019,
+			...restEntry,
+		};
+
+		return newEntry;
+	});
+
+	//console.log(cleansedData)
+	knex('entries').insert(cleansedData).then( (res) =>
+console.log(res))
 }
 
-const dailyData = scrape(CDC_URL)
+scrape(CDC_URL);
 
-console.log(dailyData)
+function insertDB(data) {
+	console.log(data);
 
- function insertDB (data) {
-
-	 console.log(data)
-
-	const cleansedData = data.map( entry => {
+	/* 	const cleansedData = data.map( entry => {
 		entry['Date'] = entry['date']
 		entry['LongName'] = entry['name']
 		entry['Census2019'] = entry['Census']
 	})
 
   knex('entries').insert(cleansedData).then( (res) =>
-console.log(res))
-
+console.log(res)) */
 }
 
-//insertDB(dailyData)
-
+//insertDB(dailyData.then( res => res))
 
 const APP_PORT = process.env.PORT || 8080;
 
@@ -52,10 +74,10 @@ app.use(
 	})
 );
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname,'public', 'index.html'));
-})
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.listen(APP_PORT, () => console.log(`App is running on ${APP_PORT}`));
