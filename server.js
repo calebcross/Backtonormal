c249c4df757750fbff0edab69c10d7fe013b7ea8
Caleb Cross <arkansashurdler@gmail.com>
2021-05-12 12:06:26 -0400

    add chron job

--- server.js
+++ server.js
@@ -2,95 +2,94 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
 const path = require("path");
+
+const cron = require('node-cron');
 const axios = require("axios");
 const knex = require("knex")(require("./knexfile"));
+const moment = require("moment");
 require("dotenv").config();
 
 const app = express();
 
+
+
 const CDC_URL =
 	"https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_data";
 
+
+
 async function scrape(URL) {
-
 	try {
-	const response = await axios.get(URL);
-	//console.log(response.data.vaccination_data)
+		const response = await axios.get(URL);
 
-	const { vaccination_data } = response.data;
+		const { vaccination_data } = response.data;
 
-	let states = null;
+		let mostRecent = vaccination_data[0].Date;
+		let todaysDate = moment(new Date()).format("YYYY-MM-DD");
 
-	knex.table("states").select("name", "id").then( res => {
-		states = res
-		//console.log(states)
-	
-	
+		console.log(`Most Recent from CDC: ${mostRecent}`)
+		
+		knex
+			.table("entries")
+			.max("date")
+			.then((results) => results[0]["max(`date`)"])
+			.then((maxDate) => {
+				console.log(`Most Recent from DB: ${maxDate}`)
+				if (maxDate != mostRecent) {
+					knex
+						.table("states")
+						.select("name", "id")
+						.then((states) => {
+							return states;
+						})
+						.then((states) => {
+							const cleansedData = vaccination_data.map((entry) => {
+								const {
+									Date,
+									LongName,
+									Census2019,
+									Location,
+									ShortName,
+									date_type,
+									Administered_Fed_LTC,
+									Administered_Fed_LTC_Dose1,
+									Administered_Fed_LTC_Dose2,
+									Series_Complete_FedLTC,
+									...restEntry
+								} = entry;
 
-		const cleansedData = vaccination_data.map((entry) => {
-			
-			const {
-				Date,
-				LongName,
-				Census2019,
-				Location,
-				ShortName,
-				date_type,
-				Administered_Fed_LTC,
-				Administered_Fed_LTC_Dose1,
-				Administered_Fed_LTC_Dose2,
-				Series_Complete_FedLTC,
-				...restEntry
-			} = entry;
+								const foundState = states.find(
+									(oneState) => oneState.name === LongName
+								);
+								const newEntry = {
+									date: Date,
+									state_id: foundState.id,
+									name: LongName,
+									Census: Census2019,
+									...restEntry,
+								};
+								return newEntry;
+							});
 
-			const foundState = states.find((oneState) => oneState.name === LongName);
-
-				//console.log(foundState)
-			const newEntry = {
-				date: Date,
-				state_id: foundState.id,
-				name: LongName,
-				Census: Census2019,
-				...restEntry,
-			};
-			return newEntry;
-		});
-
-		//console.log(cleansedData);
-		//knex('entries').insert(cleansedData).then( (res) => console.log(`inserted into table`))
-
-	})
-	}
-	catch (err) {
-		console.log(err);
-	}
-	
-}
-//knex('entries').insert(cleansedData).then( (res) => console.log(res))
-scrape(CDC_URL);
-
-const callDB = async () => {
-		const States = await knex.table("states").select("name", "id");
-		return States;
-
-};
-
-async function insertDB() {
-	let States;
-
-	try {
-		States = await knex.table("states").select("name", "id");
-		return States;
+							//console.log(cleansedData);
+							knex('entries').insert(cleansedData).then( (res) => console.log(`inserted into db`))
+						});
+				}
+				else { console.log(`not inserted`)}
+			});
 	} catch (err) {
 		console.log(err);
-		States = [];
 	}
-
-	/* knex('entries').insert(cleansedData).then( (res) =>
-console.log(res)) */
 }
 
-//insertDB(dailyData.then( res => res))
+
+
+const task = cron.schedule('* */6 * * *', () => {
+	console.log('running cdc scrape');
+	scrape(CDC_URL);
+  });
+
+  task.start();
 
 const APP_PORT = process.env.PORT || 8080;
 
